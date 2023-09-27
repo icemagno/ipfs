e88f70fc2360bb48f589fd219135d9e922e735f8
Carlos Magno Oliveira de Abreu <magno.mabreu@gmail.com>
2023-09-27 10:55:34 -0300

    Funcionou. Troquei para KUBO

--- cluster/ipfs0.sh
+++ cluster/ipfs0.sh
@@ -6,5 +6,18 @@ docker run --name ipfs0 --hostname=ipfs0 --network=sisgeodef \
 -p 5001:5001 \
 -p 8081:8080 \
 -v /srv/ipfs/ipfs0:/data/ipfs \
--d ipfs/go-ipfs:v0.4.15
+-d ipfs/kubo:release
 
+
+# ipfs bootstrap rm --all
+
+# ipfs swarm peers
+
+# Frontend ( WEBUI ) só funciona após conectar os peers e
+# Reconfigurar o acesso:
+
+# ipfs config --json API.HTTPHeaders.Access-Control-Allow-Origin '["http://172.21.81.49:5001", "http://localhost:3000", "http://127.0.0.1:5001", "https://webui.ipfs.io"]'
+# ipfs config --json API.HTTPHeaders.Access-Control-Allow-Methods '["PUT", "POST"]'
+
+# Depois precisa reiniciar o ipfs.
+# Sempre que reiniciar o IPFS, reiniciar o CLUSTER correspondente depois.
--- cluster/ipfs1.sh
+++ cluster/ipfs1.sh
@@ -2,12 +2,15 @@
 
 docker run --name ipfs1 --hostname=ipfs1 --network=sisgeodef \
 -v /etc/localtime:/etc/localtime:ro \
+-p 4001:4001 \
+-p 5001:5001 \
+-p 8081:8080 \
 -v /srv/ipfs/ipfs1:/data/ipfs \
--d ipfs/go-ipfs:bifrost-2020-02-26-a44c610
+-d ipfs/kubo:release
 
 # O Endereço é o peer id
 # ipfs bootstrap rm --all
-# ipfs bootstrap add  /ip4/10.5.112.132/tcp/4001/ipfs/12D3KooWSK3e59a7dktUGUcskjNMnyL1VEoRzCVqXPYGLYBVuW13
+# ipfs bootstrap add  /ip4/172.22.1.49/tcp/4001/ipfs/12D3KooWMe4jQUEFidcnRmEWEeY9UnsyCPeg61ygPswjFcAvQkZ5
 
 # Changing user to ipfs
 # ipfs version 0.19.0-dev
@@ -16,12 +19,12 @@ docker run --name ipfs1 --hostname=ipfs1 --network=sisgeodef \
 
 # No 132:
 # ipfs swarm peers
-# /ip4/10.5.112.133/tcp/60970/p2p/12D3KooWB2MooTU3rHdmAsUD5C5SNZJymWfBNUnkBw8oJnrMN4UW
+# /ip4/10.5.112.133/tcp/60970/p2p/12D3KooWEGXtoZwoGVc2Fm4DJ6V4zgpL8tiUzaYmJu5oFe3yERJa
 
 
 # No 133:
 # ipfs swarm peers
-# /ip4/10.5.112.132/tcp/4001/p2p/12D3KooWSK3e59a7dktUGUcskjNMnyL1VEoRzCVqXPYGLYBVuW13
+# /ip4/10.5.112.132/tcp/4001/p2p/12D3KooWMe4jQUEFidcnRmEWEeY9UnsyCPeg61ygPswjFcAvQkZ5
 
 # http://10.5.112.132:5001/webui
 
